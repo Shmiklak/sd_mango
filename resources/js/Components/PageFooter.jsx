c429ae5ba4797f8a6be5ac93d7dca9b6a3c980f8
Doston Shametov <shmiklak@gmail.com>
2024-06-07 13:55:17 +0500

    better footer

--- resources/js/Components/PageFooter.jsx
+++ resources/js/Components/PageFooter.jsx
@@ -1,7 +1,25 @@
 export const PageFooter = () => {
     return (
         <footer className="page-footer">
-            The website was developed by Shmiklak
+            The website was developed by <a href="https://osu.ppy.sh/users/5504231" target="_blank">Shmiklak</a>
+
+            <ul className="d-flex mt-3 footer-links">
+                <li>
+                    <a href="https://x.com/sd_mango_osu" target="_blank">
+                        <img src="/static/assets/images/icons/twitter.svg"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/Shmiklak/sd_mango" target="_blank">
+                        <img src="/static/assets/images/icons/github.svg"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://discord.gg/2D9aNeTuRh" target="_blank">
+                        <img src="/static/assets/images/icons/discord.svg"/>
+                    </a>
+                </li>
+            </ul>
         </footer>
     )
 }
