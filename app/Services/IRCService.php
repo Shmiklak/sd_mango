345e4f6ba034dee8940cbc58dcacbc7e307c9b65
Artrix9095 <sefu786@outlook.com>
2024-06-08 13:25:27 -0700

    Add shadcn & tailwind base styles

--- app/Services/IRCService.php
+++ app/Services/IRCService.php
@@ -23,39 +23,39 @@ class IRCService
 
     protected function connect()
     {
-        $this->socket = fsockopen($this->server, $this->port);
-        if (!$this->socket) {
-            throw new \Exception("Could not connect to IRC server.");
-        }
+        // $this->socket = fsockopen($this->server, $this->port);
+        // if (!$this->socket) {
+        //     throw new \Exception("Could not connect to IRC server.");
+        // }
     }
 
     protected function login()
     {
-        if ($this->password) {
-            $this->sendData("PASS {$this->password}");
-        }
-        $this->sendData("USER {$this->nickname} 0 * :{$this->nickname}");
-        $this->sendData("NICK {$this->nickname}");
+        // if ($this->password) {
+        //     $this->sendData("PASS {$this->password}");
+        // }
+        // $this->sendData("USER {$this->nickname} 0 * :{$this->nickname}");
+        // $this->sendData("NICK {$this->nickname}");
     }
 
     public function sendMessageToChannel($channel, $message)
     {
-        $this->sendData("JOIN {$channel}");
-        $this->sendData("PRIVMSG {$channel} :{$message}");
+        // $this->sendData("JOIN {$channel}");
+        // $this->sendData("PRIVMSG {$channel} :{$message}");
     }
 
     public function sendMessageToUser($user, $message)
     {
-        $this->sendData("PRIVMSG {$user} :{$message}");
+        // $this->sendData("PRIVMSG {$user} :{$message}");
     }
 
     protected function sendData($data)
     {
-        fwrite($this->socket, $data . "\r\n");
+        // fwrite($this->socket, $data . "\r\n");
     }
 
     public function __destruct()
     {
-        fclose($this->socket);
+        // fclose($this->socket);
     }
 }
