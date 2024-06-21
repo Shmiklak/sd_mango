be159550e604bf0c1a62576d209f0cef6460af5a
Artrix9095 <sefu786@outlook.com>
2024-06-20 22:12:03 -0700

    Fix audio player

--- resources/js/Components/Beatmap.jsx
+++ resources/js/Components/Beatmap.jsx
@@ -1,62 +1,111 @@
-import {Link} from "@inertiajs/react";
-import {BeatmapStatus} from "@/Components/BeatmapStatus.jsx";
-import {useState} from "react";
+import { Link } from "@inertiajs/react";
+import { BeatmapStatus } from "@/Components/BeatmapStatus.jsx";
+import { useEffect, useState, useRef } from "react";
+import { usePreview } from "@/Util/stores";
+import { Button } from "@/Components/ui/button";
+import { Play, Pause } from "lucide-react";
 
-export const Beatmap = ( props ) => {
-
+export const Beatmap = (props) => {
     const [cover, setCover] = useState(props.beatmap.cover);
+    const { preview, updatePreview } = usePreview((state) => state);
+
+    const isPlaying = preview.includes(String(props.beatmap.beatmapset_id));
+    return (
+        <div className="beatmap">
+            <div className="beatmap-cover">
+                <a
+                    href={`https://osu.ppy.sh/beatmapsets/${props.beatmap.beatmapset_id}`}
+                    target="_blank"
+                >
+                    <img
+                        src={cover}
+                        onError={(e) =>
+                            setCover("/static/assets/images/bg_error.png")
+                        }
+                        alt={`${props.beatmap.artist} - ${props.beatmap.title}`}
+                        className="beatmap-image"
+                    />
+                </a>
+                <Link
+                    href={route("queue_request", props.beatmap.id)}
+                    className="btn btn-primary beatmap-view-btn"
+                >
+                    <img src="/static/assets/images/icons/eye.svg" />
+                </Link>
+                <BeatmapStatus beatmap={props.beatmap} />
+            </div>
+            <div className="beatmap-text">
+                <div className="text-center mb-3">
+                    <Button
+                        onClick={() =>
+                            isPlaying
+                                ? updatePreview("")
+                                : updatePreview(props.beatmap.beatmapset_id)
+                        }
+                        variant={isPlaying ? "default" : "accent"}
+                        className="rounded-full p-2"
+                    >
+                        {isPlaying ? <Pause /> : <Play />}
+                    </Button>
+                </div>
+                <div className="beatmap-title">
+                    <a
+                        href={`https://osu.ppy.sh/beatmapsets/${props.beatmap.beatmapset_id}`}
+                        target="_blank"
+                    >
+                        {props.beatmap.artist} - {props.beatmap.title}
+                    </a>
+                </div>
+                <div className="beatmap-author">
+                    mapped by {props.beatmap.creator}
+                </div>
+                <div className="row mt-3">
+                    <div className="col-4 mt-2">
+                        <strong>Genre</strong>
+                        <br />
+                        {props.beatmap.genre}
+                    </div>
+                    <div className="col-4 mt-2">
+                        <strong>Language</strong>
+                        <br />
+                        {props.beatmap.language}
+                    </div>
+                    <div className="col-4 mt-2">
+                        <strong>BPM</strong>
+                        <br />
+                        {props.beatmap.bpm}
+                    </div>
+                    <div className="col-4 mt-2">
+                        <strong>Style</strong>
+                        <br />
+                        {props.beatmap.map_style}
+                    </div>
+                </div>
+                <p className="mt-3 mapper-comment">
+                    <strong>Mapper comment: </strong> {props.beatmap.comment}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export const Beatmaps = (props) => {
+    const { preview } = usePreview((state) => state);
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        if (preview !== "") {
+            audioRef.current.volume = 0.3;
+            audioRef.current.play();
+        }
+    }, [preview]);
 
     return (
-            <div className="beatmap">
-                <div className="beatmap-cover">
-                    <a href={`https://osu.ppy.sh/beatmapsets/${props.beatmap.beatmapset_id}`} target="_blank">
-
-                        <img src={cover}
-                             onError={(e) => setCover('/static/assets/images/bg_error.png')}
-                             alt={`${props.beatmap.artist} - ${props.beatmap.title}`}
-                             className="beatmap-image"/>
-                    </a>
-                    <Link href={route('queue_request', props.beatmap.id)} className="btn btn-primary beatmap-view-btn">
-                        <img src="/static/assets/images/icons/eye.svg"/>
-                    </Link>
-                    <BeatmapStatus beatmap={props.beatmap} />
-                </div>
-                <div className="beatmap-text">
-                    <div className="text-center mb-3">
-                        <audio controls={true}>
-                            <source src={`https://b.ppy.sh/preview/${props.beatmap.beatmapset_id}.mp3`}/>
-                        </audio>
-                    </div>
-                    <div className="beatmap-title">
-                        <a href={`https://osu.ppy.sh/beatmapsets/${props.beatmap.beatmapset_id}`} target="_blank">
-                            {props.beatmap.artist} - { props.beatmap.title }
-                        </a>
-                    </div>
-                    <div className="beatmap-author">
-                        mapped by {props.beatmap.creator}
-                    </div>
-                    <div className="row mt-3">
-                        <div className="col-4 mt-2">
-                            <strong>Genre</strong><br/>
-                            {props.beatmap.genre}
-                        </div>
-                        <div className="col-4 mt-2">
-                            <strong>Language</strong><br/>
-                            {props.beatmap.language}
-                        </div>
-                        <div className="col-4 mt-2">
-                            <strong>BPM</strong><br/>
-                            {props.beatmap.bpm}
-                        </div>
-                        <div className="col-4 mt-2">
-                            <strong>Style</strong><br/>
-                            {props.beatmap.map_style}
-                        </div>
-                    </div>
-                    <p className="mt-3 mapper-comment">
-                        <strong>Mapper comment: </strong> {props.beatmap.comment}
-                    </p>
-                </div>
-    </div>
-    )
-}
+        <>
+            {props.beatmaps.data.map((beatmap) => (
+                <Beatmap key={beatmap.id} beatmap={beatmap} auth={props.auth} />
+            ))}
+            <audio ref={audioRef} src={preview} />
+        </>
+    );
+};
--- resources/js/Pages/Queue.jsx
+++ resources/js/Pages/Queue.jsx
@@ -1,98 +1,143 @@
-import {Head, Link, useForm} from "@inertiajs/react";
+import { Head, Link, useForm } from "@inertiajs/react";
 import App from "@/Layouts/App.jsx";
 import LoginRequired from "@/Components/LoginRequired.jsx";
-import {Beatmap} from "@/Components/Beatmap.jsx";
+import { Beatmap, Beatmaps } from "@/Components/Beatmap.jsx";
 
 const Queue = ({ auth, beatmaps, title }) => {
-
     let params = new URLSearchParams(window.location.search);
     let query_url = window.location.pathname;
 
     const { data, setData, get, processing, errors, reset } = useForm({
-        map_style: params.get('map_style') === null ? 'all' : params.get('map_style'),
-        status: params.get('status') === null ? 'default' : params.get('status'),
+        map_style:
+            params.get("map_style") === null ? "all" : params.get("map_style"),
+        status:
+            params.get("status") === null ? "default" : params.get("status"),
     });
 
     const submit = (e) => {
         e.preventDefault();
         get(query_url, {
-            preserveState: true
+            preserveState: true,
         });
-    }
+    };
 
     return (
         <>
-            <Head title={title}/>
-            { auth.user === null ? (<LoginRequired/>) : (<>
-                <div className="text-center">
-                    <h1 className="section-title mb-3 text-center">{title}</h1>
-                </div>
+            <Head title={title} />
+            {auth.user === null ? (
+                <LoginRequired />
+            ) : (
+                <>
+                    <div className="text-center">
+                        <h1 className="section-title mb-3 text-center">
+                            {title}
+                        </h1>
+                    </div>
 
-                <div className="alert alert-warning">
-                    <p>
-                        If you support what we do here at <strong>sd_mango</strong> and want to shout us out in your map's description, add this to it! (This is entirely optional!)
-                    </p>
-                    <code className="mt-2 code-block">
-                        [notice][centre][size=150]<br/>
-                        🥭[url=https://x.com/sd_mango_osu][b][i]This map was brought to you by sd_mango [/b][/i][/url]🥭<br/>
-                        [/centre][/size][/notice]
-                    </code>
-                </div>
+                    <div className="alert alert-warning">
+                        <p>
+                            If you support what we do here at{" "}
+                            <strong>sd_mango</strong> and want to shout us out
+                            in your map's description, add this to it! (This is
+                            entirely optional!)
+                        </p>
+                        <code className="mt-2 code-block">
+                            [notice][centre][size=150]
+                            <br />
+                            🥭[url=https://x.com/sd_mango_osu][b][i]This map was
+                            brought to you by sd_mango [/b][/i][/url]🥭
+                            <br />
+                            [/centre][/size][/notice]
+                        </code>
+                    </div>
 
-                <div className="queue-filter">
-                    <form onSubmit={submit}>
-                        <div className="row">
-                            <div className="col-lg-4">
-                                <label>
-                                    Request status
-                                </label>
-                                <select className="form-control" value={data.status} onChange={(e) => setData('status', e.target.value)}>
-                                    <option value="default">Default</option>
-                                    <option value="PENDING">Pending</option>
-                                    <option value="ACCEPTED">Accepted</option>
-                                    <option value="NOMINATED">Nominated</option>
-                                    <option value="INVALID">Invalid</option>
-                                    <option value="HIDDEN">Hidden</option>
-                                </select>
+                    <div className="queue-filter">
+                        <form onSubmit={submit}>
+                            <div className="row">
+                                <div className="col-lg-4">
+                                    <label>Request status</label>
+                                    <select
+                                        className="form-control"
+                                        value={data.status}
+                                        onChange={(e) =>
+                                            setData("status", e.target.value)
+                                        }
+                                    >
+                                        <option value="default">Default</option>
+                                        <option value="PENDING">Pending</option>
+                                        <option value="ACCEPTED">
+                                            Accepted
+                                        </option>
+                                        <option value="NOMINATED">
+                                            Nominated
+                                        </option>
+                                        <option value="INVALID">Invalid</option>
+                                        <option value="HIDDEN">Hidden</option>
+                                    </select>
+                                </div>
+                                <div className="col-lg-4">
+                                    <label>Map style</label>
+                                    <select
+                                        className="form-control"
+                                        value={data.map_style}
+                                        onChange={(e) =>
+                                            setData("map_style", e.target.value)
+                                        }
+                                    >
+                                        <option value="all">All</option>
+                                        <option value="Jumps">Jumps</option>
+                                        <option value="Streams">Streams</option>
+                                        <option value="Difficult mechanics">
+                                            Difficult mechanics
+                                        </option>
+                                    </select>
+                                </div>
+                                <div className="col-lg-4">
+                                    <label>&nbsp;</label>
+                                    <button
+                                        disabled={processing}
+                                        className="btn btn-primary w-100"
+                                    >
+                                        Filter
+                                    </button>
+                                </div>
                             </div>
-                            <div className="col-lg-4">
-                                <label>
-                                    Map style
-                                </label>
-                                <select className="form-control" value={data.map_style} onChange={(e) => setData('map_style', e.target.value)}>
-                                    <option value="all">All</option>
-                                    <option value="Jumps">Jumps</option>
-                                    <option value="Streams">Streams</option>
-                                    <option value="Difficult mechanics">Difficult mechanics</option>
-                                </select>
-                            </div>
-                            <div className="col-lg-4">
-                                <label>&nbsp;</label>
-                                <button disabled={processing} className="btn btn-primary w-100">Filter</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {beatmaps.data.map((beatmap) => (
-                        <Beatmap key={beatmap.id} beatmap={beatmap} auth={auth}/>
-                    ))}
-                </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        <Beatmaps beatmaps={beatmaps} auth={auth} />
+                    </div>
 
-                <nav className="mt-5 mb-5">
-                    <ul className="pagination justify-content-center">
-                        {beatmaps.links.map((link) => (
-                            <li className={link.url === null ? "page-item disabled" : (link.active ? "page-item active" : "page-item")}>
-                                <Link className="page-link" href={link.url} tabIndex="-1" dangerouslySetInnerHTML={{__html: link.label}}/>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </>)}
+                    <nav className="mt-5 mb-5">
+                        <ul className="pagination justify-content-center">
+                            {beatmaps.links.map((link) => (
+                                <li
+                                    className={
+                                        link.url === null
+                                            ? "page-item disabled"
+                                            : link.active
+                                              ? "page-item active"
+                                              : "page-item"
+                                    }
+                                >
+                                    <Link
+                                        className="page-link"
+                                        href={link.url}
+                                        tabIndex="-1"
+                                        dangerouslySetInnerHTML={{
+                                            __html: link.label,
+                                        }}
+                                    />
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </>
+            )}
         </>
-    )
-}
+    );
+};
 
-Queue.layout = page => <App children={page}/>
+Queue.layout = (page) => <App children={page} />;
 export default Queue;
